8fb325fdc74b84894b9fdf1ccfbafa455210f396
Manikandan V M <manikandan@Manikandans-MacBook-Pro.local>
2022-01-18 00:09:53 +0530

    CM-4: App structure and helm configs

---
+++ src/main/java/com/k8s/cert/monitor/controller/CertMonitorController.java
@@ -0,0 +1,25 @@
+package com.k8s.cert.monitor.controller;
+
+import com.k8s.cert.monitor.service.CertMonitorService;
+import io.fabric8.certmanager.api.model.v1.Certificate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("api/v1/cert-monitor/")
+public class CertMonitorController {
+
+    private static final Logger logger = Logger.getLogger(CertMonitorController.class.getSimpleName());
+
+    @Autowired
+    private CertMonitorService certMonitorService;
+
+    @GetMapping("/certs/{namespace}/list/")
+    public List<Certificate> listCertificates(@PathVariable String namespace) {
+            return certMonitorService.listAllCertsByNamespace(namespace);
+    }
+
+}
---
+++ src/main/java/com/k8s/cert/monitor/service/CertMonitorService.java
@@ -0,0 +1,29 @@
+package com.k8s.cert.monitor.service;
+
+import io.fabric8.certmanager.api.model.v1.Certificate;
+import io.fabric8.certmanager.api.model.v1.CertificateList;
+import io.fabric8.certmanager.client.CertManagerClient;
+import io.fabric8.certmanager.client.DefaultCertManagerClient;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Logger;
+
+@Service
+public class CertMonitorService {
+
+    private static final Logger logger = Logger.getLogger(CertMonitorService.class.getSimpleName());
+
+    public List<Certificate> listAllCertsByNamespace(String namespace) {
+        try (CertManagerClient certManagerClient = new DefaultCertManagerClient()) {
+            CertificateList certificateList = certManagerClient.v1().certificates().inNamespace(namespace).list();
+            if (certificateList.getItems() != null) {
+                logger.info(String.format("Found %d Certificates in %s namespace.", certificateList.getItems().size(), namespace));
+                return certificateList.getItems();
+            } else {
+                return Collections.emptyList();
+            }
+        }
+    }
+}
